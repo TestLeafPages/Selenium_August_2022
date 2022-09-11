e19e80c1540eff08b747c7df1b520c9104259f8d
TestLeafPages <earth@testleaf.com>
2022-09-11 13:16:05 +0530

    Week 6 code

--- testcase/CreateLead.java
+++ testcase/CreateLead.java
@@ -1,24 +1,30 @@
 package testcase;
 
 import org.openqa.selenium.By;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.leaftaps.ui.base.ProjectSpecificMethods;
 
 public class CreateLead extends ProjectSpecificMethods{
-	@Test
-	public void runCreateLead() {
-		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
-		driver.findElement(By.id("password")).sendKeys("crmsfa");
+	@BeforeTest
+	public void setData() {
+		excelFileName = "tc001";
+	}
+	@Test(dataProvider = "sendData")
+	public void runCreateLead(String username, String password, String cname, String fname, String lname ) {
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
 		driver.findElement(By.className("decorativeSubmit")).click();
 		driver.findElement(By.linkText("CRM/SFA")).click();
 		driver.findElement(By.linkText("Leads")).click();
 		driver.findElement(By.linkText("Create Lead")).click();
-		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
-		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
-		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys(cname);
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys(fname);
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys(lname);
 		driver.findElement(By.name("submitButton")).click();
 }
+	
 }
 
 
--- testcase/DeleteLead.java
+++ testcase/DeleteLead.java
@@ -1,21 +1,26 @@
 package testcase;
 
 import org.openqa.selenium.By;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.leaftaps.ui.base.ProjectSpecificMethods;
 
 public class DeleteLead extends ProjectSpecificMethods {
-	@Test
-	public void runDeleteLead() throws InterruptedException {
-		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
-		driver.findElement(By.id("password")).sendKeys("crmsfa");
+	@BeforeTest
+	public void setData() {
+		excelFileName = "tc002";
+	}
+	@Test(dataProvider = "sendData")
+	public void runDeleteLead(String username, String password, String phone) throws InterruptedException {
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
 		driver.findElement(By.className("decorativeSubmit")).click();
 		driver.findElement(By.linkText("CRM/SFA")).click();
 		driver.findElement(By.linkText("Leads")).click();
 		driver.findElement(By.linkText("Find Leads")).click();
 		driver.findElement(By.xpath("//span[text()='Phone']")).click();
-		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("9");
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys(phone);
 		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
 		Thread.sleep(2000);
 		String leadID = driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).getText();
